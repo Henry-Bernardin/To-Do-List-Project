0f6c0d17d22dde4ec60f9880928f888b4fb212ec
Henry-Bernardin <henry.bernardin@gmail.com>
2019-08-12 22:50:45 -0400

    Modified click to on(click). Created input var.

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,12 +1,22 @@
 // Check off specific todos by Clicking 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
  $(this).toggleClass("completed");
     });
 
 //Clicking X will remove the todo
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove(); 
     })
     event.stopPropagation(); //stops the click listener from setting off the parent listeners
+})
+
+//Grabbing new todo text from input 
+$("input[type = 'text']").keypress(function() {
+    if (event.which === 13) {
+       let todoText = $(this).val(); 
+       $(this).val(""); //Clears out input field after text is saved to todoText variable
+       //Create a new li and append it to the ul
+       $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 })
