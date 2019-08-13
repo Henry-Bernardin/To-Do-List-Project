bb6507517623b1cf9a1abdc4a8233c3988d02dd2
Henry-Bernardin <henry.bernardin@gmail.com>
2019-08-12 20:40:01 -0400

    Added Click Listeners to the future delete button

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -2,3 +2,11 @@
 $("li").click(function(){
  $(this).toggleClass("completed");
     });
+
+//Clicking X will remove the todo
+$("span").click(function (event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove(); 
+    })
+    event.stopPropagation(); //stops the click listener from setting off the parent listeners
+})
